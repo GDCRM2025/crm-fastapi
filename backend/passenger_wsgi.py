4b6a764fefaa3e3848df68c749c3e82b90de04c6
GDCRM2025 <oscar@greendiamond.cl>
2025-10-27 15:42:18 -0300

    Delete backend directory

--- backend/passenger_wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-sys.path.insert(0, os.path.dirname(__file__))
-
-# Importa tu app ASGI desde el paquete
-from backend.main import app as application
-
-# Crea tablas si hiciera falta
-from backend.database import Base, engine
-Base.metadata.create_all(bind=engine)
