169ef8d9ea485b993d0306a043fddca3193dad3e
GDCRM2025 <oscar@greendiamond.cl>
2025-10-27 15:40:32 -0300

    Delete init_db.py

--- init_db.py
+++
@@ -1,6 +0,0 @@
-from backend.database import Base, engine
-from backend.models import Usuario, CRMGeneral
-
-print("⏳ Creando tablas en la base de datos...")
-Base.metadata.create_all(bind=engine)
-print("✅ Tablas creadas correctamente.")
